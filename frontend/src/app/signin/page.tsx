dbcd65f1b614db293e1b9bef6b221445776ba275
Fabrizzio <fabrizzio.vilchez.e@tutec.edu.pe>
2024-05-29 18:43:56 -0500

    Fix design in login

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -36,7 +36,7 @@ export default function Login() {
             </div>
             <div className="absolute h-screen w-screen bg-black bg-opacity-80 -z-10"></div>
 
-            <div className="relative h-screen w-full flex flex-col justify-center items-center space-y-4 overflow-x-clip px-5">
+            <div className="relative h-screen w-full flex flex-col justify-center items-center space-y-4 overflow-x-clip px-5 py-5">
                 <div className="flex flex-row text-white items-center space-x-5 justify-center">
                     <Image
                         src="/assets/logo/logo.png"
@@ -49,42 +49,44 @@ export default function Login() {
                         GPT GAMES
                     </h2>
                 </div>
-                <div className="font-inter bg-gradient-to-tr from-fuchsia-900 to-violet-800 py-4 sm:py-10 px-10 sm:px-20 rounded-2xl w-full max-w-2xl overflow-y-hidden">
-                    <form className="flex flex-col gap-4 w-full items-start" onSubmit={handleSubmit(onSubmit)}>
-                        <label className="text-white w-full">Email</label>
+                <div className="font-inter bg-gradient-to-tr from-fuchsia-900 to-violet-800 py-4 sm:py-8 px-10 sm:px-20 rounded-2xl w-full max-w-2xl overflow-y-scroll custom-scroll">
+                    <form className="flex flex-col gap-2 w-full items-start" onSubmit={handleSubmit(onSubmit)}>
+                        <label className="text-white w-full text-sm">Email</label>
                         <input
                             {...register('email', { required: 'Email is required', pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' } })}
                             type="email"
                             placeholder="example@mail.com"
-                            className="p-3 rounded-2xl bg-gray-800/40 text-white w-full"
+                            className="p-2 rounded-2xl bg-gray-800/40 text-white w-full"
                         />
-                        {errors.email && <span className="text-red-500 text-md">{errors.email.message}</span>}
+                        {errors.email && <span className="text-red-500 text-sm md:text-md">{errors.email.message}</span>}
 
-                        <label className="text-white w-full">Password</label>
+                        <label className="text-white w-full text-sm">Password</label>
                         <input
-                            {...register('password', { required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters long' } })}
+                            {...register('password', { required: 'Password is required', minLength: { value: 8, message: 'Password must be at least 8 characters long' } })}
                             type="password"
                             placeholder="********"
-                            className="p-3 rounded-2xl bg-gray-800/40 text-white w-full"
+                            className="p-2 rounded-2xl bg-gray-800/40 text-white w-full"
                         />
-                        {errors.password && <span className="text-red-500 text-md">{errors.password.message}</span>}
+                        {errors.password && <span className="text-red-500 text-sm md:text-md">{errors.password.message}</span>}
 
-                        <button className="font-bold p-2 mt-2 w-full bg-black text-white rounded-2xl">
+                        <button className="font-bold p-2 mt-2 w-full bg-black text-white rounded-2xl text-sm md:text-md">
                             Log in
                         </button>
                     </form>
-                    <div className="flex flex-col justify-center items-center gap-4 w-full mt-2">
+                    <div className="flex flex-col justify-center items-center gap-2 w-full mt-2">
                         <div className="flex items-center w-full">
                             <hr className="flex-grow border-t border-gray-300" />
                             <span className="mx-2 text-gray-300">or</span>
                             <hr className="flex-grow border-t border-gray-300" />
                         </div>
                         <button className="flex items-center justify-center w-full p-2 bg-white rounded-2xl shadow-lg space-x-4">
-                            <FcGoogle className="text-4xl" />
-                            <h2 className="text-black font-bold">Sign in with Google</h2>
+                            <FcGoogle className="text-2xl" />
+                            <h2 className="text-black font-bold text-sm md:text-md">Sign in with Google</h2>
                         </button>
-                        <a href="#" className="text-white underline">Forgot your password?</a>
-                        <a href="#" className="text-white underline">Create an account</a>
+                        <div className="flex flex-row w-full items-center justify-evenly mt-2">
+                            <a href="#" className="text-white text-sm underline">Forgot your password?</a>
+                            <a href="#" className="text-white text-sm underline">Create an account</a>
+                        </div>
                     </div>
                 </div>
             </div>
