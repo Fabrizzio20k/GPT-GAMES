435a58d0cc18689ab139871ed0d7152f1fdf4629
Jeffrey Monja Castro <jeffrey.monja@utec.edu.pe>
2024-06-11 19:42:41 -0500

    Fetching offers

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,15 +1,80 @@
 "use client"
 
+import React, { useEffect, useState } from "react";
 import Offer from "@/components/Offer";
 import MainLayoutPage from "@/pages/MainLayoutPage";
 import { useAppSelector } from "@/redux/store";
 import { Button } from "@/components/ui/button";
 import { CiCirclePlus } from "react-icons/ci";
 import { useRouter } from "next/navigation";
+import { toast, Toaster } from "sonner";
+import axios from "axios";
 
 export default function Dashboard() {
+    const [userOffers, setUserOffers] = useState([] as any);
+    const [userOffersInfo, setUserOffersInfo] = useState([] as any);
     const user = useAppSelector((state) => state.user);
     const router = useRouter();
+    const urlServer = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
+
+    const fetchUserOffers = async () => {
+        try {
+            const response = await axios.get(urlServer + "/profile/", {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `token ${user.token}`,
+                }
+            })
+
+            setUserOffers(response.data.user.offers);
+            
+        } catch (error) {
+            const listErrors = (error as any).response.data;
+            let errors = '';
+            for (const key in listErrors) {
+                errors += `${listErrors[key]}\n`;
+            }
+            errors = errors.toUpperCase();
+            toast.error(errors);
+        }
+    }
+
+    const fetchOffers = async (url: string) => {
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `token ${user.token}`,
+                }
+            })
+
+            setUserOffersInfo((prevOffers: any) => [...prevOffers, response.data]);
+
+        } catch (error) {
+            const listErrors = (error as any).response.data;
+            let errors = '';
+            for (const key in listErrors) {
+                errors += `${listErrors[key]}\n`;
+            }
+            errors = errors.toUpperCase();
+            toast.error(errors);
+        }
+    }
+
+    const showOffers = async () => {
+        userOffers.map((offer: any) => {
+            fetchOffers(offer)
+        })
+
+        console.log(userOffersInfo);
+        
+    }
+    
+
+    useEffect(() => {
+        fetchUserOffers();
+        showOffers();
+    }, []);
 
     return (
         <MainLayoutPage>
@@ -34,6 +99,10 @@ export default function Dashboard() {
                     <span className="ml-2">Publish offer</span>
                 </Button>
             </section>
+
+            <Button onClick={showOffers}>
+                Show offers
+            </Button>
             
 
             <section className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
