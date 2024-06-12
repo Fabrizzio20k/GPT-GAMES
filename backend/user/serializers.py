2154371db6249df8b4c2ecbbaba083120f10daba
Fabrizzio20k <fabrizzio785@gmail.com>
2024-06-11 19:50:50 -0500

    Add extra files

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from user.models import User
-from billing_info.serializers import BillingInfoSerializer  # Importa el serializer de BillingInfo
+# Importa el serializer de BillingInfo
+from billing_info.serializers import BillingInfoSerializer
 from offer.serializers import OfferSerializer
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     billing_info = serializers.HyperlinkedRelatedField(
@@ -12,7 +14,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     offers = serializers.HyperlinkedRelatedField(
         many=True,
-        view_name='offer-detail',  # Asegúrate de que este nombre coincida con el registrado en el router
+        # Asegúrate de que este nombre coincida con el registrado en el router
+        view_name='offer-detail',
         read_only=True
     )
 
@@ -21,11 +24,21 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         view_name='shoppingcar-detail',
         read_only=True
     )
+
     class Meta:
         model = User
-        fields = ['url', 'id', 'username', 'first_name', 'last_name', 'description', 'email', 'phone', 'billing_info', 'offers' , 'password'
-            ,'shopping_car']
+        fields = ['url', 'id', 'username', 'first_name', 'last_name', 'description',
+                  'email', 'phone', 'billing_info', 'offers', 'password', 'shopping_car']
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
