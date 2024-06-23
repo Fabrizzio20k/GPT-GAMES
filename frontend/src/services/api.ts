e99b128b7803e9ad798665f72358a9612c3b5859
Fabrizzio20k <fabrizzio785@gmail.com>
2024-06-23 10:40:49 -0500

    Relocate fetch functions

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { User } from "@/types/user";
+
+const urlServer = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
 
 const searchGamesByName = async (name: string) => {
-  const urlServer = process.env.NEXT_PUBLIC_DEV_SERVER_URL;
-
   try {
     const response = await axios.get(urlServer + `/games/?search=${name}`);
 
@@ -12,3 +13,53 @@ const searchGamesByName = async (name: string) => {
     return [];
   }
 }
+
+const registerUser = async (data: any) => {
+
+  let errors = [];
+  let dataResponse = {};
+
+  try {
+    const response = await axios.post(urlServer + "/register/", data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    dataResponse = response.data;
+  } catch (error) {
+    errors = (error as any).response.data;
+  }
+
+  return { errors, dataResponse };
+}
+
+const loginUser = async (data: any) => {
+  let dataUser = {} as User;
+  let errors = [];
+
+  try {
+    const response = await axios.post(urlServer + "/login/", data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    dataUser = {
+      id: response.data.user.id,
+      email: response.data.user.email,
+      username: response.data.user.username,
+      first_name: response.data.user.first_name,
+      last_name: response.data.user.last_name,
+      phone: response.data.user.phone,
+      description: response.data.user.description,
+      token: response.data.token,
+    } as User;
+
+  } catch (error) {
+    errors = (error as any).response.data;
+  }
+
+  return { errors, dataUser };
+}
+
+export { searchGamesByName, registerUser, loginUser };
