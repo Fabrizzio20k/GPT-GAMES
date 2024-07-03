1d8c53e7dfa16d5431855b1094cb4800b68dc9e0
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-07-02 21:25:13 -0500

    Add drag and drop

--- frontend/src/app/viewprofile/[id]/page.tsx
+++ frontend/src/app/viewprofile/[id]/page.tsx
@@ -3,7 +3,7 @@
 import MainLayoutPage from "@/pages/MainLayoutPage";
 import { useAppSelector } from "@/redux/store";
 import { toast, Toaster } from "sonner";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Loader from "@/components/Loader";
 import { useRouter, useParams } from "next/navigation";
 import Image from "next/image";
@@ -12,6 +12,8 @@ import { User } from "@/types/user";
 import { getUserById } from "@/services/api";
 import OfferBySeller from "@/interfaces/OfferBySeller";
 import OfferNew from "@/components/OfferNew";
+import { useDropzone } from 'react-dropzone';
+import { FaUpload } from "react-icons/fa";
 
 
 export default function ViewProfile() {
@@ -27,8 +29,14 @@ export default function ViewProfile() {
         router.push('/editprofile');
     }
 
-    useEffect(() => {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        console.log("Uploaded file:", acceptedFiles[0].name);
+        // Aquí puedes agregar la lógica para subir la imagen al servidor.
+    }, []);
 
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
             if (!user.token) {
@@ -65,7 +73,26 @@ export default function ViewProfile() {
             <article className="w-full flex flex-col gap-8">
                 <section className="w-full flex flex-col md:flex-row gap-8 items-center md:items-start justify-between">
                     <div className="flex flex-col xs:flex-row items-center w-full md:w-fit gap-8">
-                        <Image src="/assets/background/d2.jpg" alt="user-photo" width={1920} height={1080} className="rounded-2xl w-[200px] border-gray-300 border-4" />
+                        <div
+                            {...getRootProps()}
+                            className={`relative rounded-2xl w-[200px] h-[200px] border-gray-300 border-4 flex items-center justify-center ${isDragActive ? 'bg-gray-200' : ''}`}
+                            style={{ cursor: user.id.toString() === showedUser.id.toString() ? 'pointer' : 'default' }}
+                        >
+                            <input {...getInputProps()} />
+                            <Image
+                                src="/assets/background/d2.jpg"
+                                alt="user-photo"
+                                width={1920}
+                                height={1080}
+                                className="rounded-2xl w-full h-full object-cover"
+                            />
+                            {user.id.toString() === showedUser.id.toString() && (
+                                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 hover:opacity-100 transition-opacity p-4 space-x-4 rounded-2xl">
+                                    <FaUpload className="text-4xl" />
+                                    <span className="ml-2 text-xl">Click or Drop to upload</span>
+                                </div>
+                            )}
+                        </div>
                         <div className="font-alata space-y-4">
                             <h1 className="font-bold tracking-widest text-2xl">@{showedUser.username}</h1>
                             <h2 className="text-xl">{showedUser.first_name} {showedUser.last_name}</h2>
