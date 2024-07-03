57788311ddf3f1daf3271b3e95c0cb19af2f1b97
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-07-02 21:06:02 -0500

    Add frontend files

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -14,7 +14,7 @@ const statePersistConfig = {
 const userPersistConfig = {
     key: "user",
     storage: storage,
-    whitelist: ["id", "email", "username", "first_name", "last_name", "phone", "description", "token", "offers"],
+    whitelist: ["id", "email", "profile_photo", "username", "first_name", "last_name", "phone", "description", "token", "offers"],
 };
 
 const rootReducer = combineReducers({
--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -3,6 +3,7 @@ import OfferBySeller from "@/interfaces/OfferBySeller";
 export type User = {
     id: string;
     email: string;
+    profile_photo: string;
     username: string;
     first_name: string;
     last_name: string;
